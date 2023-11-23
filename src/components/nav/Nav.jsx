c5e1c2ee806a76bbf42cc412f176cf88b41eeb21
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-23 18:14:34 +0500

    I added codes to Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,6 +4,9 @@ import { FaShoppingCart } from "react-icons/fa";
 const Nav = () => {
   return (
     <nav>
+      <div className="nav__logo">
+        <h1>Platzi Fake Store</h1>
+      </div>
       <ul className="nav-menu">
         <li>
           <NavLink className={({ isActive }) => isActive ? "link--active" : "isActive"} to="/">Home</NavLink>
@@ -12,12 +15,15 @@ const Nav = () => {
           <NavLink className={({ isActive }) => isActive ? " link--active" : "isActive"} to="about">About</NavLink>
         </li>
         <li>
-          <NavLink className={({ isActive }) => isActive ? "link--active" : "isActive"} to="product-cart">Carts <FaShoppingCart /></NavLink>
+          <NavLink className={({ isActive }) => isActive ? " link--active" : "isActive"} to="about">Contact Us</NavLink>
+        </li>
+        <li>
         </li>
         
       </ul>
 
-      <div className="profil-user">
+      <div className="profile-user">
+      <NavLink className={({ isActive }) => isActive ? "link--active" : "isActive"} to="product-cart">Carts <FaShoppingCart /></NavLink>
           <Link to='create-product'>Create Product</Link>
       </div>
     </nav>
