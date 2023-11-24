d897642834cb431cc915d3a888020584f7cf1173
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-24 21:15:13 +0500

    Bug fixed

--- src/routes/sub-routes/sign-up/SignUp.jsx
+++ src/routes/sub-routes/sign-up/SignUp.jsx
@@ -1,8 +1,24 @@
 import "./SignUp.scss"
-
+import { Link } from "react-router-dom"
 const SignUp = () => {
     return (
-        <div>SignUp</div>
+        <div className="signup__wrapper">
+        <h2>SIGN UP</h2>
+        <form className="signup-form">
+          <div className="login-email">
+            <label htmlFor="Email">Email</label>
+            <input id="Email" type="email" />
+          </div>
+          <div className="login-password">
+            <label htmlFor="Password">Password</label>
+            <input id="Password" type="password" />
+          </div>
+          <button>Login</button>
+          <p className="checkout-text">
+            If You don't have an account ? <Link to='auth/login'>Login</Link>
+          </p>
+        </form>
+      </div>
     )
 }
 
