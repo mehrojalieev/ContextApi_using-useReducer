66e278536bab2c9786f94e2c12f5857d022074c6
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-24 21:26:43 +0500

    Finished SignUp page

--- src/routes/sub-routes/sign-up/SignUp.jsx
+++ src/routes/sub-routes/sign-up/SignUp.jsx
@@ -1,25 +1,33 @@
 import "./SignUp.scss"
 import { Link } from "react-router-dom"
 const SignUp = () => {
-    return (
-        <div className="signup__wrapper">
-        <h2>SIGN UP</h2>
-        <form className="signup-form">
-          <div className="login-email">
-            <label htmlFor="Email">Email</label>
-            <input id="Email" type="email" />
-          </div>
-          <div className="login-password">
-            <label htmlFor="Password">Password</label>
-            <input id="Password" type="password" />
-          </div>
-          <button>Login</button>
-          <p className="checkout-text">
-            If You don't have an account ? <Link to='auth/login'>Login</Link>
-          </p>
-        </form>
-      </div>
-    )
+  return (
+    <div className="signup__wrapper">
+      <h2>SIGN UP</h2>
+      <form className="signup-form">
+        <div className="signup-name">
+          <label htmlFor="Name">Name</label>
+          <input id="Name" type="text" />
+        </div>
+        <div className="signup-email">
+          <label htmlFor="Email">Email</label>
+          <input id="Email" type="email" />
+        </div>
+        <div className="signup-password">
+          <label htmlFor="Password">Password</label>
+          <input id="Password" type="password" />
+        </div>
+        <div className="signup-avatar">
+          <label htmlFor="Avatar">Avatar</label>
+          <input id="Avatar" type="text" />
+        </div>
+        <button>Login</button>
+        <p className="checkout-text">
+          If You  have an account ? <Link to='/auth/login'>Login</Link>
+        </p>
+      </form>
+    </div>
+  )
 }
 
 export default SignUp   
