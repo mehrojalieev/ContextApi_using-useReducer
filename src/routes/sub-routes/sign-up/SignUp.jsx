0b07e5df13d77eefb2f76746a64fdb8a3b3beda4
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-24 21:38:33 +0500

    Done signup FETCH successfully

--- src/routes/sub-routes/sign-up/SignUp.jsx
+++ src/routes/sub-routes/sign-up/SignUp.jsx
@@ -1,27 +1,55 @@
+import { useState } from "react"
 import "./SignUp.scss"
 import { Link } from "react-router-dom"
+
+
+
 const SignUp = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [avatar, setAvatar] = useState('')
+
+  const handleCreateUser = (e) => {
+    e.preventDefault()
+    fetch('https://api.escuelajs.co/api/v1/users/', {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+        avatar: avatar
+      }),
+      headers:{
+        "Content-Type": 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   return (
     <div className="signup__wrapper">
       <h2>SIGN UP</h2>
-      <form className="signup-form">
+      <form onSubmit={handleCreateUser} className="signup-form">
         <div className="signup-name">
           <label htmlFor="Name">Name</label>
-          <input id="Name" type="text" />
+          <input value={name} onChange={(e) => setName(e.target.value)} id="Name" type="text" />
         </div>
         <div className="signup-email">
           <label htmlFor="Email">Email</label>
-          <input id="Email" type="email" />
+          <input value={email} onChange={(e) => setEmail(e.target.value)} id="Email" type="email" />
         </div>
         <div className="signup-password">
           <label htmlFor="Password">Password</label>
-          <input id="Password" type="password" />
+          <input value={password} onChange={(e) => setPassword(e.target.value)} id="Password" type="password" />
         </div>
         <div className="signup-avatar">
           <label htmlFor="Avatar">Avatar</label>
-          <input id="Avatar" type="text" />
+          <input value={avatar} onChange={(e) => setAvatar(e.target.value)} id="Avatar" type="text" />
         </div>
-        <button>Login</button>
+        <button type="submit">Sign Up</button>
         <p className="checkout-text">
           If You  have an account ? <Link to='/auth/login'>Login</Link>
         </p>
