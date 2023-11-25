f7672be9e86d94d61ddd1a1177c18d397d1aca50
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-25 23:52:42 +0500

    Planes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,12 @@ import "./Header.scss"
 const Header = () => {
   return (
     <header>
-        
+        <h2>Header</h2>
+        <pre>
+            Connect Swiper:
+            Add Product:
+            Give better Styles:
+        </pre>
     </header>
   )
 }
