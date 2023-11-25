45197195739ab8caaf3dbff6de4a8343b564f576
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-25 23:51:05 +0500

    Connected

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./home.scss"
 import Main from '../../components/main/Main'
+import Header from "../../components/header/Header"
 
 const Home = () => {
     
     return (
         <>
+        <Header/>
             <Main/>
         </>
     )
