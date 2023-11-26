b757088e8487f389c1171a9a2710cb4b602e92ca
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-26 09:15:42 +0500

    Connected Axios to the LoginPage

--- src/routes/sub-routes/login/Login.jsx
+++ src/routes/sub-routes/login/Login.jsx
@@ -1,46 +1,53 @@
 import { useRef, useState } from "react"
 import "./Login.scss"
 import { Link } from "react-router-dom"
+import { apiInstance } from "../../../api"
 
 const Login = () => {
-
-  const localName = localStorage.getItem("name")
-  const localEmail = localStorage.getItem("email")
 
   const [loadingBtn, setLoadingBtn] = useState(false)
   const loginbtn = useRef()
   const LoginBtn = loginbtn.current
 
-  const [loginName, setLoginName] = useState('')
   const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
 
   const handleLogin = (e) => {
     e.preventDefault()
-    if (loginName === localName && loginEmail === localEmail) {
-      setLoadingBtn(true)
-      LoginBtn.style = "cursor: not-allowed; opacity: 0.6; background: transparent; color: #fff;"
-      setTimeout(() => { window.location.pathname = "/" }, 1700)
-      console.log(true);
-    } else {
-      console.log(false);
-    }
+    apiInstance.post('/auth/login/', {
+      password: loginPassword,
+      email: loginEmail
+    })
+      .then(response => {
+        if (response.data.access_token) {
+          console.log(response);
+          setLoadingBtn(true)
+          LoginBtn.style = "cursor: not-allowed; opacity: 0.6; background: transparent; color: #fff;"
+          localStorage.setItem("user-token", response.data.access_token)
+          setTimeout(() => { window.location.pathname = "/" }, 1700)
+        }
+        else {
+          alert("Password or Email is Invalid !!!")
+        }
+      })
+      .catch(error => console.log(error))
   }
 
   return (
     <div className="login__wrapper">
       <h2>Login</h2>
       <form onSubmit={handleLogin} className="login-form">
-        <div className="login-name">
-          <label htmlFor="Name">Name</label>
-          <input value={loginName} onChange={(e) => setLoginName(e.target.value)} id="Password" type="text" />
-        </div>
         <div className="login-email">
           <label htmlFor="Email">Email</label>
           <input value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)} id="Email" type="email" />
         </div>
+        <div className="login-name">
+          <label htmlFor="Name">Password</label>
+          <input value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} id="Password" type="password" />
+        </div>
         <button ref={loginbtn} type="submit">Login</button>
         {/* Loaidng */}
-        <div style={loadingBtn ? {display:"block", cursor: "not-allowed"} : {display: "none"}} className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        <div style={loadingBtn ? { display: "block", cursor: "not-allowed" } : { display: "none" }} className="lds-ring"><div></div><div></div><div></div><div></div></div>
         {/*  */}
         <p className="checkout-text">
           If You don't have an account ? <Link to='/auth/signup'>Sign Up</Link>
