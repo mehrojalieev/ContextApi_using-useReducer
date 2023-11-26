7092ca95e79a90bca9128dc04a18e869d91c5076
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-26 22:08:51 +0500

    Changed file-structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './scss/App.scss'
+import './App.scss'
 import Nav from './layout/nav/Nav'
 import AllRoutes from "./routes/AllRoutes"
 // import  { SkeletonTheme } from 'react-loading-skeleton'
